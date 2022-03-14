bc2a6d5351a96ea39ab52801d7ebb3c82623bf93
satyan_p. joshi <100898570+Satyanjoshi@users.noreply.github.com>
2022-03-14 09:55:58 +0530

    Final upload

---
+++ Audit_managment_system/MFPE(14032022)/audit-checklist/src/main/java/com/mfpe/model/AuditType.java
@@ -0,0 +1,21 @@
+package com.mfpe.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@ToString
+@Getter
+@Setter
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuditType {
+	private String auditType;
+	
+}
