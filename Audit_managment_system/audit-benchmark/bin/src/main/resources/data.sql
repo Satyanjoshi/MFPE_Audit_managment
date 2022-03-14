8429f81f6726d9bc6c4ab97e7399d232005374e0
satyan_p. joshi <100898570+Satyanjoshi@users.noreply.github.com>
2022-03-14 09:43:44 +0530

    update

--- Audit_managment_system/audit-benchmark/bin/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO audit_benchmark(benchmark_id,audit_type,benchmark_no_answers) VALUES (1,'Internal',3);
-INSERT INTO audit_benchmark(benchmark_id,audit_type,benchmark_no_answers) VALUES (2,'SOX',1);
