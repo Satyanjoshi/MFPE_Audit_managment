e919320894eb6810d23cf84d54de8883bbf9fdf3
satyan_p. joshi <100898570+Satyanjoshi@users.noreply.github.com>
2022-03-14 11:36:34 +0530

    removeall

--- Audit_managment_system/MFPE(14032022)/audit-benchmark/src/main/java/com/mfpe/repository/AuditBenchmarkRepo.java
+++
@@ -1,9 +0,0 @@
-package com.mfpe.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mfpe.model.AuditBenchmark;
-
-public interface AuditBenchmarkRepo extends JpaRepository<AuditBenchmark, Integer> {
-	
-}
